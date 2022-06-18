5ba863842f60cf21bd87b2283fc5cdbbd3bda947
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-06-18 12:27:28 +0800

    Create AppRouter.js

---
+++ src/router/AppRouter.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from '../views/home/Home.js'
+import ReactPage from '../views/home/ReactPage.js'
+import VuePage from '../views/home/VuePage.js'
+import Demo1 from '../views/demo1/Demo1.js'
+import Demo2 from '../views/demo2/Demo2.js'
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        {/* 首页使用精确匹配 exact 首页嵌套路由*/}
+        <Route path="/" exact element={<Home />}>
+          {/* {索引路由是父路由的默认子路由} */}
+          <Route index element={
+            <main>
+              <p>Select an index</p>
+            </main>} />
+          <Route path="reactPage" element={<ReactPage />} />
+          <Route path="vuePage" element={<VuePage />} />
+        </Route>
+
+        <Route path="demo1/:id" element={<Demo1 />} />
+        <Route path="demo2" element={<Demo2 />} />
+            
+        <Route
+          path="*"
+          element={
+            <main>
+              <h2>404 Not Found</h2>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
+    </Router>
+  )
+}
+
+export default AppRouter
